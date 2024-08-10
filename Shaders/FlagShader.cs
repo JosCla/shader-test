4019705eceaeccd86f168da5c65c416d0a1f897b
JosCla <jozerclaver@gmail.com>
2024-08-10 15:58:47 -0600

    Made flag shader use a simpler function to generate waves
    Yeah, putting multiple springs in series is a great way to get massive instability. I'm just gonna generate the windy waves by hand.

--- Shaders/FlagShader.cs
+++ Shaders/FlagShader.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -5,28 +6,39 @@ namespace shader_test
 {
     public class FlagShader : OurShader
     {
-        private PhysicsChain _chain;
+        public static float WAVE_PROPORTION = 0.06f;
+        public static int WAVE_NUM_SEGS = 15;
 
-        public FlagShader() {
-            this._chain = new PhysicsChain(new Vector2(200.0f, 200.0f), 10, 20.0f);
-        }
+        public static float WAVE_END_TIME_MULT = 1.2f;
+        public static float WAVE_INNER_TIME_MULT = 1.5f;
 
-        public override void Update(float timeElapsed)
-        {
-            base.Update(timeElapsed);
-
-            _chain.Update(timeElapsed);
-        }
+        public FlagShader() {}
 
         public override void Draw(float timeElapsed, GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
-            System.Console.WriteLine("frame");
-            foreach (Vector2 link in _chain.GetLinkPoses()) {
-                System.Console.WriteLine(link);
+            for (int i = 0; i <= WAVE_NUM_SEGS; i++) {
+                Vector2 pos = GetFlagPos(
+                    new Vector2(200.0f, 200.0f),
+                    new Vector2(200.0f, 50.0f),
+                    (float)i / (float)WAVE_NUM_SEGS
+                );
+
                 spriteBatch.Draw(
                     _texture,
-                    link,
+                    pos,
+                    null,
+                    Color.White,
+                    0.0f,
+                    Vector2.Zero,
+                    new Vector2(0.1f, 0.1f),
+                    SpriteEffects.None,
+                    0.0f
+                );
+
+                spriteBatch.Draw(
+                    _texture,
+                    pos + new Vector2(0.0f, 100.0f),
                     null,
                     Color.White,
                     0.0f,
@@ -37,6 +49,30 @@ namespace shader_test
                 );
             }
             spriteBatch.End();
+        }
+
+        private Vector2 GetFlagPos(Vector2 anchorPos, Vector2 lengthVec, float proportion)
+        {
+            // finding the size of flag waves
+            float waveLen = lengthVec.Length() * WAVE_PROPORTION;
+
+            // making end point wave around a bit
+            Vector2 endPos = anchorPos + lengthVec;
+            endPos += new Vector2(
+                (float)Math.Cos(WAVE_END_TIME_MULT * _totalTime),
+                (float)Math.Sin(WAVE_END_TIME_MULT * _totalTime)
+            ) * waveLen;
+
+            // making vertex wave around a bit
+            Vector2 orthogonalVec = new Vector2(-lengthVec.Y, lengthVec.X);
+            orthogonalVec.Normalize();
+            orthogonalVec *= waveLen;
+            Vector2 waveOffset = (float)Math.Sin(proportion * 5.0f + _totalTime * WAVE_INNER_TIME_MULT) * orthogonalVec;
+
+            // finding final position
+            return (anchorPos * (1.0f - proportion))
+                + (endPos * proportion)
+                + waveOffset;
         }
     }
 }
