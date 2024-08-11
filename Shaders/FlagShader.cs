c9321193d9f5f8b8e43a405356a980d205b20365
JosCla <jozerclaver@gmail.com>
2024-08-11 17:57:27 -0600

    Added simple banner for flag shader testing

--- Shaders/FlagShader.cs
+++ Shaders/FlagShader.cs
@@ -7,15 +7,19 @@ namespace shader_test
 {
     public class FlagShader : OurShader
     {
-        public static float WAVE_PROPORTION = 0.06f;
-        public static int WAVE_NUM_SEGS = 15;
+        public static readonly float WAVE_PROPORTION = 0.06f;
+        public static readonly int WAVE_NUM_SEGS = 15;
 
-        public static float WAVE_END_TIME_MULT = 1.2f;
-        public static float WAVE_INNER_TIME_MULT = 1.5f;
+        public static readonly float WAVE_END_TIME_MULT = 2.4f;
+        public static readonly float WAVE_INNER_TIME_MULT = 3.0f;
 
-        public static float FLAG_HEIGHT = 100.0f;
+        public static readonly float FLAG_HEIGHT = 100.0f;
+
+        public static readonly Vector2 FLAG_ANCHOR_POS = new Vector2(100.0f, 100.0f);
 
         private Effect _flagShader;
+
+        private Vector2 _windDir;
 
         public FlagShader() {}
 
@@ -24,6 +28,22 @@ namespace shader_test
             base.LoadContent(content);
 
             _flagShader = content.Load<Effect>("Shaders/FlagShader");
+        }
+
+        public override void Update(float timeElapsed)
+        {
+            base.Update(timeElapsed);
+
+            if (InputUtils.IsMouseHeld()) {
+                Vector2 mouseDir = new Vector2(
+                    InputUtils.GetBoundedMousePos().X,
+                    InputUtils.GetBoundedMousePos().Y * 2.0f - 1.0f
+                );
+                mouseDir.Normalize();
+
+                float texAspectRatio = (float)_texture.Width / (float)_texture.Height;
+                _windDir = mouseDir * FLAG_HEIGHT * texAspectRatio;
+            }
         }
 
         public override void Draw(float timeElapsed, GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
@@ -45,14 +65,14 @@ namespace shader_test
                 float rightU = (float)rightUPixel / (float)_texture.Width;
 
                 Vector2 pos = Vector2.Floor(GetFlagPos(
-                    new Vector2(200.0f, 200.0f),
-                    new Vector2(200.0f, 50.0f),
+                    FLAG_ANCHOR_POS,
+                    _windDir,
                     leftU
                 ));
 
                 Vector2 nextPos = Vector2.Floor(GetFlagPos(
-                    new Vector2(200.0f, 200.0f),
-                    new Vector2(200.0f, 50.0f),
+                    FLAG_ANCHOR_POS,
+                    _windDir,
                     rightU
                 ));
 
@@ -60,9 +80,8 @@ namespace shader_test
                 _flagShader.Parameters["leftSideU"]?.SetValue(leftU);
                 _flagShader.Parameters["rightSideU"]?.SetValue(rightU);
 
-                float fullWidth = (nextPos.X - pos.X) * (float)WAVE_NUM_SEGS;
                 Vector2 scale = new Vector2(
-                    fullWidth / _texture.Width,
+                    (float)(nextPos.X - pos.X) / (float)(rightUPixel - leftUPixel),
                     FLAG_HEIGHT / _texture.Height
                 );
 
@@ -97,19 +116,36 @@ namespace shader_test
             Vector2 endPos = anchorPos + lengthVec;
             endPos += new Vector2(
                 (float)Math.Cos(WAVE_END_TIME_MULT * _totalTime),
-                (float)Math.Sin(WAVE_END_TIME_MULT * _totalTime)
+                // (float)Math.Sin(WAVE_END_TIME_MULT * _totalTime)
+                0.0f
             ) * waveLen;
 
             // making vertices wave around a bit
             Vector2 orthogonalVec = new Vector2(-lengthVec.Y, lengthVec.X);
             orthogonalVec.Normalize();
             orthogonalVec *= waveLen;
+            orthogonalVec *= (float)Math.Sqrt(proportion);
             Vector2 waveOffset = (float)Math.Sin(proportion * 5.0f + _totalTime * WAVE_INNER_TIME_MULT) * orthogonalVec;
 
             // finding final position
             return (anchorPos * (1.0f - proportion))
                 + (endPos * proportion)
                 + waveOffset;
+        }
+
+        public override string GetTexturePath()
+        {
+            return "sample-banner";
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            _windDir = new Vector2(
+                FLAG_HEIGHT * 4.685f,
+                FLAG_HEIGHT * 1.0f
+            );
         }
     }
 }
